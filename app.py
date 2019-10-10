e85a002542dd783945c7610d13245bb3fca26bb4
Yinan Ye <yinan-ye@uiowa.edu>
2019-10-10 15:02:56 -0500

    Final work I think, good job for me

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Loan Calculator')
+    return render_template('index.html', display="", pageTitle='Loan Calculator')
+
+@app.route('/payment', methods=['GET', 'POST'])
+def payments():
+    if request.method == 'POST':
+        form = request.form
+        A = int(form['A'])
+        n = int(form['n'])
+        i = int(form['i'])
+        calc = A / (((( 1 + ( i * 0.001) ) **n ) - 1 ) / ( ( i * 0.001) * ( 1 + ( i * 0.001) ) **n ))
+        return render_template('index.html', display=calc, pageTitle='Loan Calculator')
+
+    return redirect("/")
+
 
 @app.route('/yinan')
 def yinan():
